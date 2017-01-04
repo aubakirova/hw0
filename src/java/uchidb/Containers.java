204206db266d70f2df7229d499e4506ee7bd7204
Aaron Elmore <aelmore@csail.mit.edu>
2017-01-04 12:36:48 -0600

    First pass

---
+++ src/java/uchidb/Containers.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package uchidb;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author aelmore
+ * A simple interface to wrap some basic Java containers
+ * This class uses:
+ * - generics (T and S) https://en.wikipedia.org/wiki/Generics_in_Java
+ * - containers  (sets,lists, and maps) http://beginnersbook.com/2015/01/difference-between-list-set-and-map-in-java/
+ * - overloaded functions
+ */
+public interface Containers<T, S> {
+
+	//Create a set based on the array of T objects
+	public Set<T> initSet(T[] tArray);
+	
+	//Create a list based on the array of T objects
+	public List<T> initList(T[] tArray);
+	
+	//Create a map based on the array of T objects
+	public Map<S,T> initMap();
+	
+	//Store the map in a local field variable -- often called a setter 
+	public void storeMap(Map<S,T> mapToStoreInClass);
+	
+	//add a key value to store map with a boolean indicating whether to overwrite existing value
+	public boolean addToMap(S key, T value, boolean overwriideExistingValue);
+	
+	//get a value based on a key
+	public T getValueFromMap(S key);
+	
+	//an overloaded function to get value from map but with a default value
+	public T getValueFromMap(S key, T defaultValue);
+}
---
+++ src/java/uchidb/HW0Runner.java
@@ -0,0 +1,22 @@
+package uchidb;
+
+
+/**
+ * @author aelmore
+ */
+public class HW0Runner {
+
+	//TODO you likely will need to add member variable
+	
+	// This class is a factory for a singleton containers class.
+	// https://www.tutorialspoint.com/java/java_using_singleton.htm
+	public static Containers<Integer, String> getContainers() {
+		// TODO fix this function
+		return null;
+	}
+
+
+	public static void main(String[] args){
+		
+	}
+}
