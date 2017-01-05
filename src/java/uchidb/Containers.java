02d1382b80ee70fc02fe7f0c53e01d6e8fea7936
Aaron Elmore <aaron.elmore@gmail.com>
2017-01-04 18:18:11 -0600

    fix to function names and comments

--- src/java/uchidb/Containers.java
+++ src/java/uchidb/Containers.java
@@ -17,24 +17,25 @@ import java.util.Set;
  */
 public interface Containers<T, S> {
 
-	//Create a set based on the array of T objects
+	//Create a set that stores the array of T objects
 	public Set<T> initSet(T[] tArray);
 	
-	//Create a list based on the array of T objects
+	//Create a list that stores an the array of T objects
 	public List<T> initList(T[] tArray);
 	
-	//Create a map based on the array of T objects
-	public Map<S,T> initMap();
+	//Create an empty map that will use type S as keys, and T as values
+	public Map<S,T> initEmptyMap();
 	
 	//Store the map in a local field variable -- often called a setter 
 	public void storeMap(Map<S,T> mapToStoreInClass);
 	
 	//add a key value to store map with a boolean indicating whether to overwrite existing value
-	public boolean addToMap(S key, T value, boolean overwriideExistingValue);
+	public boolean addToMap(S key, T value, boolean overwriteExistingKey);
 	
 	//get a value based on a key
 	public T getValueFromMap(S key);
 	
 	//an overloaded function to get value from map but with a default value
+	//if the key is not present
 	public T getValueFromMap(S key, T defaultValue);
 }
