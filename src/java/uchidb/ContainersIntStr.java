263bd944bfa219a85d71432ce2718840410b705f
Sun77789 <aubakirova.m.m@gmail.com>
2017-01-05 22:06:07 -0600

    Almost done

---
+++ src/java/uchidb/ContainersIntStr.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package uchidb;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * @author aubakirova
+ */
+
+public class ContainersIntStr implements Containers<Integer, String> {
+	private Map<String,Integer> map = null;
+
+	//Create a set that stores the array of T objects
+	public Set<Integer> initSet(Integer[] tArray) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i =0; i < tArray.length; i++){
+			set.add(tArray[i]);
+		}
+		return set;
+	}
+	
+	//Create a list that stores an the array of T objects
+	public List<Integer> initList(Integer[] tArray) {
+		List<Integer> list = new LinkedList<Integer>();
+		for (int i =0; i < tArray.length; i++){
+			list.add(tArray[i]);
+		}
+		return list;
+	}
+	
+	//Create an empty map that will use type S as keys, and T as values
+	public Map<String,Integer> initEmptyMap() {
+		return new HashMap<String,Integer>();
+	}
+	
+	//Store the map in a local field variable -- often called a setter 
+	public void storeMap(Map<String,Integer> mapToStoreInClass) {
+		map = mapToStoreInClass;
+	}
+	
+	//add a key value to store map with a boolean indicating whether to overwrite existing value
+	public boolean addToMap(String key, Integer value, boolean overwriteExistingKey) {
+		if (map.containsKey(key) && map.get(key) != value && !overwriteExistingKey)
+			return false;
+		map.put(key, value);
+		return true;
+	}
+	
+	//get a value based on a key
+	public Integer getValueFromMap(String key) {
+		return map.get(key);
+	}
+	
+	//an overloaded function to get value from map but with a default value
+	//if the key is not present
+	public Integer getValueFromMap(String key, Integer defaultValue) {
+		if (!map.containsKey(key))
+			return defaultValue;
+		return map.get(key);
+	}
+}
--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -7,12 +7,13 @@ package uchidb;
 public class HW0Runner {
 
 	//TODO you likely will need to add member variable
+	private static Containers<Integer, String> cs = new ContainersIntStr();
 	
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
 	public static Containers<Integer, String> getContainers() {
 		// TODO fix this function
-		return null;
+		return cs;
 	}
 
 
